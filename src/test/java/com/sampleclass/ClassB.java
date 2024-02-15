0e3318c9af61c208f1805acff73a28d6581607c4
merinjos08 <gowthamimerin1993@gmail.com>
2024-02-15 11:15:56 +0530

    New Project Automation COmmit

---
+++ src/test/java/com/sampleclass/ClassB.java
@@ -0,0 +1,15 @@
+package com.sampleclass;
+
+public class ClassB {
+
+	private void java() {
+		System.out.println("JAVA");
+
+	}
+
+	private void selenium() {
+		System.out.println("SEL");
+
+	}
+
+}
